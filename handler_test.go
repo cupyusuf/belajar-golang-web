bf94aa34499a848806b1ae52cff210e1f39c47d0
Yusuf Supriadi <yusufdolenk2@gmail.com>
2025-03-03 17:44:37 +0700

    feat: add handler

---
+++ handler_test.go
@@ -0,0 +1,21 @@
+package belajar_golang_web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World")
+	}
+	server := http.Server{
+		Addr:    "localhost:9090",
+		Handler: handler,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
