845bf5e17b55926f9770c70305d7795b87f90868
Yusuf Supriadi <yusufdolenk2@gmail.com>
2025-03-04 10:18:28 +0700

    feat: add template XSS

--- xss_test.go
+++ xss_test.go
@@ -66,3 +66,32 @@ func TestTemplateAutoEscapeDisabledServer(t *testing.T) {
 		panic(err)
 	}
 }
+
+func TemplateXSS(writer http.ResponseWriter, request *http.Request) {
+	myTemplates.ExecuteTemplate(writer, "post.gohtml", map[string]interface{}{
+		"Title": "Template Auto Escape",
+		"Body":  template.HTML(request.URL.Query().Get("body")),
+	})
+}
+
+func TestTemplateXSS(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:9090/?body=<p>Alert</p>", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateXSS(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TestTemplateXSSServer(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:9090",
+		Handler: http.HandlerFunc(TemplateXSS),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
