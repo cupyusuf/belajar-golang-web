95737faa600c61d1e1aca85708e874ad4f73c345
Yusuf Supriadi <yusufdolenk2@gmail.com>
2025-03-04 09:27:44 +0700

    feat: add template function

---
+++ template_function_test.go
@@ -0,0 +1,35 @@
+package belajar_golang_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"text/template"
+)
+
+type MyPage struct {
+	Name string
+}
+
+func (myPage MyPage) SayHello(name string) string {
+	return "Hello " + name + ", My name is " + myPage.Name
+}
+
+func TemplateFunction(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.New("FUNCTION").Parse(`{{.SayHello "Firlana"}}`))
+	t.ExecuteTemplate(writer, "FUNCTION", MyPage{
+		Name: "Yusuf",
+	})
+}
+
+func TestTemplateFunction(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:9090", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateFunction(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
