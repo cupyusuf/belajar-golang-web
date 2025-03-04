11133a3b8ba184f5be6c8be6ea08c52009386022
Yusuf Supriadi <yusufdolenk2@gmail.com>
2025-03-04 11:43:19 +0700

    feat: add middleware

---
+++ middleware_test.go
@@ -0,0 +1,44 @@
+package belajar_golang_web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+type LogMiddleware struct {
+	Handler http.Handler
+}
+
+func (middleware *LogMiddleware) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	fmt.Println("Before Execute Handler")
+	middleware.Handler.ServeHTTP(writer, request)
+	fmt.Println("After Execute Handler")
+}
+
+func TestMiddleware(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Println("Handler Executed")
+		fmt.Fprint(writer, "Hello Middleware")
+	})
+
+	mux.HandleFunc("/foo", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Println("Handler Executed")
+		fmt.Fprint(writer, "Hello Foo")
+	})
+
+	logMiddleware := &LogMiddleware{
+		Handler: mux,
+	}
+
+	server := http.Server{
+		Addr:    "localhost:9090",
+		Handler: logMiddleware,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
