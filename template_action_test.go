5431ddf5b9c6c12e7b4ab565e963c6563bb5907a
Yusuf Supriadi <yusufdolenk2@gmail.com>
2025-03-04 08:20:50 +0700

    feat: add template if

---
+++ template_action_test.go
@@ -0,0 +1,28 @@
+package belajar_golang_web
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TemplateActionIf(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/if.gohtml"))
+	t.ExecuteTemplate(writer, "if.gohtml", Page{
+		Title: "Template Data Struct",
+		Name:  "Yusuf",
+	})
+}
+
+func TestTemplateActionIf(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:9090", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateActionIf(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
